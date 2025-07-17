6386e116d48389b98f1d53c7f6dab9850bbec7c2
MSanathoi <sanathoimoirangthem552@gmail.com>
2025-07-17 14:09:10 +0530

    Added POST API to create new Donor

--- backend/controllers/donorController.js
+++ backend/controllers/donorController.js
@@ -10,3 +10,18 @@ const getAllDonors = async (req, res) => {
 };
 
 module.exports = { getAllDonors };
+
+const createDonor = async (req, res) => {
+  try {
+    const newDonor = new Donor(req.body);
+    const savedDonor = await newDonor.save();
+    res.status(201).json(savedDonor);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getDonors,
+  createDonor,
+};
